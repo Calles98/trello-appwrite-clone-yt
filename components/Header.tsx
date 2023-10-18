c4ce05affd448fb55e034858779487a8da77fec1
Rodrigo Calles <joserodrigoc98@gmail.com>
2023-10-18 11:07:04 -0700

    finishing board and adding gpt functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,38 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from "next/image"
 import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/20/solid'
 import Avatar from 'react-avatar'
+import { useBoardStore } from '@/store/BoardStore'
+import fetchSuggestion from '@/lib/fetchSuggestion'
 
 const Header = () => {
+
+    const [board, searchString, setSearchString] = useBoardStore((state) => [
+        state.board, 
+        state.searchString,
+        state.setSearchString, 
+    ]); 
+
+    const [loading, setLoading ] = useState<boolean>(false); 
+    const [suggestion, setSuggestion] = useState<any[]>([]); 
+
+    useEffect(() => {
+        if (board.columns.size === 0) return; 
+        setLoading(true); 
+
+        const fetchSuggestionFunc = async () => {
+            const suggestion = await fetchSuggestion(board); 
+            setSuggestion(suggestion); 
+            setLoading(false); 
+        }
+        
+        setTimeout(() => {
+            fetchSuggestionFunc()
+          }, 3000);
+
+    }, [board])
   return (
     <header>
         <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl'>
@@ -40,7 +67,7 @@ const Header = () => {
                 {/* Search box */}
                 <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
                     <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
-                    <input type="text" placeholder='search' className='flex-1 outline-none p-2' />
+                    <input type="text" placeholder='search' className='flex-1 outline-none p-2' value={searchString} onChange={e => setSearchString(e.target.value)} />
                     <button type='submit' hidden>search</button>
                 </form>
 
@@ -50,11 +77,11 @@ const Header = () => {
         </div>
 
         <div className='flex items-center justify-center px-5 py-2 md:py-5'>
-            <p className='flex items-center p-5 text-small font-light shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]'>
-                <UserCircleIcon className='inline-block h-10 w-10 text-[#0055D1] mr-1' />
-                GPT is summerising your tasks for the day...
-            </p>
-        </div>
+        <p className="bg-white rounded-md shadow-md p-2 text-[#0055D1]">
+            <UserCircleIcon className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${loading && "animate-spin"}`} />
+            {(suggestion.length !==0) ? `You have ${(suggestion[0]===1) ? "1 task to do" : `${suggestion[0]} tasks to do`}, ${(suggestion[1]===1) ? "1 task in progress" : `${suggestion[1]} tasks in progress `} and ${(suggestion[2]===1) ? "1 task done" : `${suggestion[2]} tasks done`}` : "We are summarising your tasks for the day"}
+        </p>
+        </div> 
     </header>
   )
 }
